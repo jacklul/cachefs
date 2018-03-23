4ed52ddc43f87a8032587043a56ded5ae818ee22
Jack'lul <jacklulcat@gmail.com>
2018-03-23 21:52:25 +0100

    Update header

--- src/CacheFS.php
+++ src/CacheFS.php
@@ -1,8 +1,11 @@
 <?php
 /**
- * This file is part of the DST Server List (GAE) project.
+ * This file is part of the CacheFS project.
  *
  * (c) Jack'lul <jacklulcat@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace jacklul\CacheFS;
